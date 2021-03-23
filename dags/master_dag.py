b44cde3c5bbb67acc61c18e2f909c0a338d798e4
Adhithya-Karthikeyan <81232373+Adhithya-Karthikeyan@users.noreply.github.com>
2021-03-23 21:32:15 +0530

    Update master_dag.py

--- dags/master_dag.py
+++ dags/master_dag.py
@@ -6,13 +6,15 @@ import os
 import sys 
 import json
 
-#sys.path.insert(0, os.path.abspath(os.path.dirname('__file__')))
 sys.path.append(os.getcwd()+'/airflow/Scripts/')
 sys.path.append(os.getcwd()+'/airflow/inputs/')
 
+# Importing the external python modules 
 from data_preparation import data_preparation
 from data_processing import data_processing
 
+
+# Arguments required for Dag run
 
 default_args = {
     'owner': 'Adhithya_Karthikeyan',
@@ -24,6 +26,10 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
     # 'queue': 'bash_queue',
 }
+
+# Dag creation
+# Dag name - data_prep_and_process
+
 dag = DAG(
     'data_prep_and_process',
     default_args=default_args,
@@ -36,7 +42,7 @@ dag = DAG(
 with open(os.getcwd()+'/airflow/inputs/inputs.json', 'r') as file:
     runtime_args = eval(file.read())
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
+# Getting the runtime arguments from the input json
 data_preparation_target_file_system = runtime_args['data_preparation_target_file_system']
 data_preparation_target_path = runtime_args['data_preparation_target_path']
 data_preparation_target_file_name = runtime_args['data_preparation_target_file_name']
@@ -45,11 +51,10 @@ data_preparation_s3_bucket = runtime_args['data_preparation_s3_bucket']
 data_processing_json_read_path = runtime_args['data_processing_json_read_path']
 data_processing_psql_creds = runtime_args['data_processing_psql_creds']
 
-
-
 data_preparation_runtime_args = [data_preparation_target_file_system, data_preparation_target_path, data_preparation_target_file_name, data_preparation_s3_creds, data_preparation_s3_bucket]
 data_processing_runtime_args = [data_processing_json_read_path, data_processing_psql_creds]
 
+# Creating task (t1) --> For data_preperation (reads JSON from URL, Write to local drive/S3, Filter records)
 t1 = PythonOperator(
     task_id='data_preparation',
     provide_context=True,
@@ -58,6 +63,11 @@ t1 = PythonOperator(
     dag=dag,
     do_xcom_push = False
 )
+
+"""
+    Creating task (t2) --> For data_processing and Warehousing 
+   (Reads JSON file from local drive using PySpark, Removes duplicates, Creates currency and historical records table, Pushes tables to PostgreSQL)
+"""
 
 t2 = PythonOperator(
     task_id='data_processing',
@@ -68,7 +78,7 @@ t2 = PythonOperator(
     do_xcom_push = False
 )
 
-
+# Creating task priorities to run in sequencial order
 
 t1 >> t2
 
